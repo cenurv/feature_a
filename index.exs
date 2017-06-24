fdb7a69bb0a7bec703b1b3ac8133c7189022d175
Joseph Lindley <josephl@tazworks.com>
2017-06-23 22:19:16 -0600

    Update index.exs

--- index.exs
+++ index.exs
@@ -2,7 +2,7 @@ use Dynaload
 
 require_script :extension
 
-IO.inspect "Loaded script (updated from github)"
+IO.inspect "Loaded script (updated from github again)"
 
 defmodule FeatureA.Index do
   def hello(name), do: "Hello #{name}"
